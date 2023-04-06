f9b39d3bd174c5e04722559d494d35b9efa6b688
Webber Woo <bird23074035@gmail.com>
2023-04-06 10:43:15 +0800

    Feature: Configure routing

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-const https = require('https');
-const express = require('express');
-const app = express();
+const https = require("https")
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
+const TOKEN = process.env.LINE_ACCESS_TOKEN
 
-app.use(express.json());
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 
-app.get('/', (req, res) => {
-    res.sendStatus(200);
-});
+app.get("/", (req, res) => {
+    res.sendStatus(200)
+})
 
-const PORT = process.env.PORT || 3000;
-const TOKEN = process.env.LINE_ACCESS_TOKEN;
+app.post("/webhook", function (req, res) {
+    res.send("HTTP POST request sent to the webhook URL!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
