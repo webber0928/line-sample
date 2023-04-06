f1457cad795d08f077d80adbb7fc36d0215f4079
Webber Woo <bird23074035@gmail.com>
2023-04-06 10:41:55 +0800

    Feature: Configure routing

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const https = require("https")
-const express = require("express")
-const app = express()
+const https = require('https');
+const express = require('express');
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
-const PORT = process.env.PORT || 3000
-const TOKEN = process.env.LINE_ACCESS_TOKEN
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+});
 
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
