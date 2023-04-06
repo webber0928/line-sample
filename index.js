ee9eef26ab8824fe7d2d15a1f1c1953a1387862a
Webber Woo <bird23074035@gmail.com>
2023-04-06 10:40:58 +0800

    Feature: Configure middleware

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const https = require("https")
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
 const PORT = process.env.PORT || 3000
 const TOKEN = process.env.LINE_ACCESS_TOKEN
 
